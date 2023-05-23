c81fcd38ba4b5528768ce6b5ce0b50e05d26d4e3
Said Dinar Fadilah <said.fadilah@indocyber.id>
2023-05-23 19:21:16 +0700

    main class

--- src/main/java/com/githubaction/GithubActionApplication.java
+++ src/main/java/com/githubaction/GithubActionApplication.java
@@ -2,10 +2,15 @@ package com.githubaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
-
+@RestController
 @SpringBootApplication
+@RequestMapping(value = "/")
 public class GithubActionApplication {
 	private static Logger logger = Logger.getLogger(GithubActionApplication.class.getName());
 
@@ -13,5 +18,10 @@ public class GithubActionApplication {
 		SpringApplication.run(GithubActionApplication.class, args);
 
 		logger.info("Logging an INFO-level message");
+	}
+	@RequestMapping(value = "/{endpoint}", method = RequestMethod.GET)
+	String getHello(@PathVariable("endpoint") String endpoint){
+		logger.info(endpoint);
+		return "Hello Spring Boot";
 	}
 }
